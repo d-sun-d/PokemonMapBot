f632f3f971d3a18c66eac5b751736e9a98d3cc3c
Alexander Ryabchenko <d-sun-d@yandex-team.ru>
2016-07-23 19:28:31 +0300

    add detect location to behaviour

--- main.py
+++ main.py
@@ -50,12 +50,24 @@ def hodor(token):
         validate(request.json, update_schema)
     except ValidationError as detail:
         abort(400, detail.args[0])
-    app.logger.info('chat id={0}, json=\n{1}'.format(request.json["message"]["chat"]["id"],
-                                                     pprint.pformat(request.json)))
-    res = {
-        'chat_id': request.json["message"]["chat"]["id"],
-        'text': HODOR_QUOTES[randint(0, len(HODOR_QUOTES) - 1)]
-    }
+
+    message = request.json["message"].get("text", "<None>")
+    location = request.json["message"].get("location")
+    app.logger.info('chat id={0}, text={1}, location={2}'.format(
+        request.json["message"]["chat"]["id"],
+        str(message),
+        str(location))
+    )
+    if location is None:
+        res = {
+            'chat_id': request.json["message"]["chat"]["id"],
+            'text': "Send me location"
+        }
+    else:
+        res = {
+            'chat_id': request.json["message"]["chat"]["id"],
+            'text': "your location: "+str(location)
+        }
     requests.post('https://api.telegram.org/bot{0}/SendMessage'.format(os.environ.get('TELEGRAM_TOKEN')), data=res)
     return jsonify(res), 200
 
